9b63306162daa58fa5a66af47343b6a40af1dae5
eundeang <111877048+eundeang@users.noreply.github.com>
2023-12-18 21:38:25 +0900

    init: 초기 프로젝트 파일 세팅

---
+++ Bliss_BE/src/test/java/org/example/bliss_be/BlissBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bliss_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlissBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
