0967a09fbfe056fc35db10eb41e50d89d38eb3a3
eundeang <111877048+eundeang@users.noreply.github.com>
2023-12-20 01:33:28 +0900

    Fix: 컨플릭트 해결

--- src/main/java/Repository/TreeRepository.java
+++
@@ -1,9 +0,0 @@
-package Repository;
-
-import Entity.Tree;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TreeRepository extends JpaRepository<Tree, Long> {
-}
