d1ed2a3120fe8a234012aa520cd865b6a4d23080
eundeang <111877048+eundeang@users.noreply.github.com>
2023-12-20 00:45:19 +0900

    Feat: Add join,login 로그인, 회원가입 기능 구현

---
+++ src/main/java/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import dto.MemberDTO;
+import entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity,String> {
+}
