93ee9c45bd44d1646df4c83df0b3a488005387c5
aapdo <jade04342@gmail.com>
2023-12-20 15:55:04 +0900

    Fix: ornament over view

--- src/main/java/org/example/bliss_be/dto/TreeOverViewDTO.java
+++ src/main/java/org/example/bliss_be/dto/TreeOverViewDTO.java
@@ -18,16 +18,39 @@ public class TreeOverViewDTO {
     private List<Boolean> ornamentOverViewList;
 
     public void setOrnamentIdList(Tree tree) {
+        List<Boolean> tempOverViewList = new ArrayList<>();
+
+        List<Ornament> ornamentList = tree.getOrnamentList();
+        System.out.println("test");
+
+        for (Ornament o : ornamentList) {
+            if (o.getIsGoodMemory() == null) {
+                tempOverViewList.add(false);
+                log.info("ornament is not null");
+            } else {
+                tempOverViewList.add(true);
+                log.info("ornament is null");
+            }
+
+        }
+
+        this.ornamentOverViewList = tempOverViewList;
+    }
+    /*
+
+    public void setOrnamentIdList(Tree tree) {
         this.ornamentOverViewList = new ArrayList<>();
         Ornament ornament = Ornament.builder()
                 .id(null)
                 .isGoodMemory(null)
                 .memory(null)
                 .tree(null).build();
+        List<Ornament> ornamentList = tree.getOrnamentList();
+        log.info("ornamentList.size() = {}", ornamentList.size());
+        for (int i = 0; i < 13; i++) {
+            log.info("ornament0 = {}" , ornamentList.get(i));
 
-        for (int i = 0; i < tree.getOrnamentList().size(); i++) {
-            log.info("ornament0 = {}" , tree.getOrnamentList().get(0));
-            if (tree.getOrnamentList().get(i).equals(ornament)) {
+            if (ornamentList.get(i).getId() == null ) {
                 this.ornamentOverViewList.add(false);
                 log.info("ornament is not null");
             } else {
@@ -36,4 +59,6 @@ public class TreeOverViewDTO {
             }
         }
     }
+
+     */
 }
