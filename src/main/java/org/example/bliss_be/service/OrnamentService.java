062deeee49ef5bec19ef7e7fbba4bdb84d8f9240
aapdo <jade04342@gmail.com>
2023-12-20 16:27:14 +0900

    fix add ornament

--- src/main/java/org/example/bliss_be/service/OrnamentService.java
+++ src/main/java/org/example/bliss_be/service/OrnamentService.java
@@ -9,6 +9,7 @@ import org.example.bliss_be.entity.Tree;
 import org.example.bliss_be.repository.OrnamentRepository;
 import org.example.bliss_be.repository.TreeRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,17 +34,21 @@ public class OrnamentService {
         }
         return  ornamentDTOList;
     }
+
+    @Transactional
     public Integer addOrnament(OrnamentDTO ornamentDTO){
         Boolean edited = false;
         int listIndex = -1;
-        // 1, 3, 5, 10 ,15
-        // 1, 5, 8,
+        int[] tmpIdx = new int[5]; tmpIdx[0] = 1; tmpIdx[1] = 3; tmpIdx[2] = 5; tmpIdx[3] = 8; tmpIdx[4] = 11;
+        // 1, 3, 5, 8, 11 -> special
+        // 나머지가 노말
         Optional<Tree> myTree = treeRepository.findById(ornamentDTO.getTreeId());
         Tree tree;
-        List<Ornament> getOrnermentList;
+        List<Ornament> ornamentList;
+        int insertIdx = 0;
         if(myTree.isPresent()){
             tree = myTree.get();
-
+            ornamentList = tree.getOrnamentList();
             int badNum = tree.getNumBadMemory();
             int goodNum = tree.getNumGoodMemory();
             log.info("goodNum = {}, badNum = {}", goodNum, badNum);
@@ -52,6 +57,15 @@ public class OrnamentService {
             if(ornamentDTO.getIsGoodMemory()){
                 goodNum++;
                 edited = true;
+                while(true) {
+                    if (ornamentList.get(insertIdx).getIsGoodMemory() == null) {
+                        break;
+                    } else if (insertIdx > 13) {
+                        edited = false;
+                        break;
+                    }
+                    insertIdx++;
+                }
             }else {
                 badNum++;
                 if (badNum == 1 || badNum == 3 || badNum == 5 || badNum == 10 || badNum == 15) {
@@ -60,9 +74,24 @@ public class OrnamentService {
                 } else {
                     System.out.println("나쁜기억을 더 만들어오세요");
                 }
+
+                insertIdx = 0;
+                for (int i = 0; i < 5; i++) {
+                    insertIdx = tmpIdx[i];
+                    if (ornamentList.get(insertIdx).getIsGoodMemory() == null) {
+                        break;
+                    }
+                }
+                if (insertIdx > 12) {
+                    edited = false;
+                }
+
+                //1, 3, 5, 8, 11
             }
             tree.setNumGoodMemory(goodNum);
             tree.setNumBadMemory(badNum);
+            log.info("insert idx = {}", insertIdx);
+
 
 
             if(!edited){
@@ -77,12 +106,20 @@ public class OrnamentService {
                         .tree(tree)
                         .build();
 
+                Ornament oldOrnament = ornamentList.get(insertIdx);
+                ornamentRepository.deleteById(oldOrnament.getId());
+
+                ornamentList.set(insertIdx, ornament);
                 ornamentRepository.save(ornament);
+
+                //tree.setOrnamentList(ornamentList);
+                //Tree newTree = treeRepository.save(tree);
+
                 System.out.println("기본 오너먼트가 만들어졌어요");
 
-                getOrnermentList = tree.getOrnamentList();
+                //ornamentList = tree.getOrnamentList();
 
-                listIndex = getOrnermentList.indexOf(ornament);
+                listIndex = insertIdx;
                 log.info("list index = {}", listIndex);
             }
         }else {
